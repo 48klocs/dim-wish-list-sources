89a825a4396e661ed326f6ba64fb0882c338c214
nev <gh@f-m.fm>
2021-10-14 22:35:48 -0700

    overhaul node tools

--- node-utils/src/clean-rolls.ts
+++ node-utils/src/clean-rolls.ts
@@ -1,115 +1,64 @@
-import d2manifest from "@d2api/manifest";
-import { WishListRoll } from "./lib/types.js";
+import d2manifest from "@d2api/manifest-node";
+// import { WishListRoll } from "./lib/types.js";
 import { toDimWishListRoll } from "./lib/wishlist-file.js";
 import fs from "fs";
+// import fsa from "fs/promises";
 import readline from "readline";
-import { toWishList } from "./lib/wishlist-file.js";
-import { DestinyInventoryItemDefinition } from "bungie-api-ts/destiny2";
+// import { toWishList } from "./lib/wishlist-file.js";
+// import { DestinyInventoryItemDefinition } from "bungie-api-ts/destiny2";
+import { isInvalidRoll } from "./utils.js";
 
-const verboseMain = true;
-const placesToLookForValidPerks: (
-  | "randomizedPlugSetHash"
-  | "reusablePlugSetHash"
-)[] = ["randomizedPlugSetHash", "reusablePlugSetHash"];
+export const verboseMain = true;
+await d2manifest.load();
 
-(async () => {
-  let counter = 0;
-  await d2manifest.load();
-  ["Mercules904", "PandaPaxxy", "AyyItsChevy", "blueberries-dot-gg", "YeezyGT" /*, "misc"*/].forEach((dirName) => {
-    fs.readdirSync("../" + dirName).forEach(async (fileName) => {
-      if (!fileName.includes(".txt") || fileName.includes("-clean.txt")) {
-        return;
-      }
+[
+  "Mercules904",
+  "PandaPaxxy",
+  "AyyItsChevy",
+  "blueberries-dot-gg",
+  "YeezyGT",
+  // "misc",
+].forEach(async (dirName) =>
+  fs.readdirSync("../" + dirName).forEach(async (fileName) => {
+    if (!fileName.includes(".txt") || fileName.includes("-clean.txt")) {
+      return;
+    }
 
-      verboseMain && console.log(`\nloading ${fileName}`);
+    verboseMain && console.log(`\nloading ${fileName}`);
 
-      const sourceFileName = "../" + dirName + "/" + fileName;
-      const fileStream = fs.createReadStream(sourceFileName);
+    const sourceFileName = "../" + dirName + "/" + fileName;
+    const sourceFileStream = fs.createReadStream(sourceFileName);
 
-      const cleanFileName = sourceFileName.replace(".txt", "-clean.txt");
-      const cleanFileStream = fs.createWriteStream(
-        "../" + dirName + "/" + cleanFileName,
-        {
-          flags: "a",
-        }
-      );
+    const cleanFileName = sourceFileName.replace(".txt", "-clean.txt");
+    const cleanFileStream = fs.createWriteStream(
+      "../" + dirName + "/" + cleanFileName,
+      { flags: "a" }
+    );
 
-      const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity,
-      });
+    const rl = readline.createInterface({
+      input: sourceFileStream,
       // Note: we use the crlfDelay option to recognize all instances of CR LF
       // ('\r\n') in input.txt as a single line break.
+      crlfDelay: Infinity,
+    });
 
-      for await (const line of rl) {
-        // Each line in input.txt will be successively available here as `line`.
-        if (line.includes("dimwishlist:")) {
-          const dimWishListRoll = toDimWishListRoll(line);
-          if (isValidWishListRoll(dimWishListRoll)) {
-            cleanFileStream.write(line + "\n");
-          }
-        } else {
+    for await (const line of rl) {
+      // Each line in input.txt will be successively available here as `line`.
+      if (line.includes("dimwishlist:")) {
+        const dimWishListRoll = toDimWishListRoll(line);
+        if (dimWishListRoll && isInvalidRoll(dimWishListRoll)) {
           cleanFileStream.write(line + "\n");
         }
+      } else {
+        cleanFileStream.write(line + "\n");
       }
+    }
 
-      fileStream.destroy();
-      cleanFileStream.end();
+    sourceFileStream.destroy();
+    cleanFileStream.end();
 
-      fs.rename(cleanFileName, sourceFileName, (err) => {
-        if (err) throw err;
-      });
+    fs.rename(cleanFileName, sourceFileName, (err) => {
+      if (err) throw err;
     });
-  });
-})();
-
-// given an array of [valid roll, invalid roll, valid roll, invalid roll]
-// returns           [false,      1,            false,      3           ]
-// so i guess you have an array of the indices of invalid rolls
-function isValidWishListRoll(roll: WishListRoll | null, verbose = false) {
-  if (!roll) {
-    return false;
-  }
-
-  const perksOnThisRoll = [...roll.recommendedPerks];
-  const rollItem = getItem(roll.itemHash);
-
-  if (!rollItem) {
-    return false;
-  }
-
-  const itemName = rollItem.displayProperties.name;
-  if (isDummy(rollItem)) {
-    return false;
-  }
-
-  const perksThisGunCanActuallyHave = (rollItem.sockets?.socketEntries
-    .reduce((acc, se) => {
-      const hashesInPlugsets = placesToLookForValidPerks.reduce(
-        (inneracc, key) =>
-          inneracc.concat(
-            getPlugSet(se[key] ?? -99999999)?.reusablePlugItems?.map(
-              (p) => p.plugItemHash
-            )
-          ),
-        [] as (number | undefined)[]
-      );
-      const reusablePlugItemHashes = se.reusablePlugItems.map(
-        (pi) => pi.plugItemHash
-      );
-      return acc.concat([...hashesInPlugsets, ...reusablePlugItemHashes]);
-    }, [] as (number | undefined)[])
-    .filter(Boolean) ?? []) as number[];
-  return perksOnThisRoll.every((p) => perksThisGunCanActuallyHave.includes(p));
-}
-
-// ItemCategory [3109687656] "Dummies"
-function isDummy(item: DestinyInventoryItemDefinition) {
-  return item?.itemCategoryHashes?.includes(3109687656);
-}
-function getItem(hash: number) {
-  return d2manifest.get("DestinyInventoryItemDefinition", hash);
-}
-function getPlugSet(hash: number) {
-  return d2manifest.get("DestinyPlugSetDefinition", hash);
-}
+  })
+);
--- node-utils/src/validate-rolls.ts
+++ node-utils/src/validate-rolls.ts
@@ -1,115 +1,56 @@
-import d2manifest from "@d2api/manifest";
-import { WishListRoll } from "./lib/types.js";
 import fs from "fs";
 import { toWishList } from "./lib/wishlist-file.js";
-import { DestinyInventoryItemDefinition } from "bungie-api-ts/destiny2";
+import { isInvalidRoll } from "./utils.js";
+import fetch from "cross-fetch";
 
-const verboseMain = true;
-const placesToLookForValidPerks: (
-  | "randomizedPlugSetHash"
-  | "reusablePlugSetHash"
-)[] = ["randomizedPlugSetHash", "reusablePlugSetHash"];
+let counter = 0;
 
-(async () => {
-  let counter = 0;
-  await d2manifest.load();
-  ["Mercules904", "PandaPaxxy", "AyyItsChevy", "blueberries-dot-gg", "YeezyGT" /*, "misc"*/].forEach(dirName => {
-    fs.readdirSync("../" + dirName).forEach(fileName => {
-      verboseMain && console.log(`\nloading ${fileName}`);
-      const rolls = toWishList(
-        fs.readFileSync("../" + dirName + "/" + fileName, 'utf-8')
-      ).wishListRolls;
-      const badIndexes = getInvalidWishlistRolls(rolls, verboseMain);
-      const badRolls = badIndexes.filter(i => i !== false).length;
-      verboseMain &&
-        console.log(`ran ${badIndexes.length} rolls -- ${badRolls} bad rolls`);
-      counter += badRolls;
-      verboseMain && console.log(`bad wishlist lines so far: ${counter}`);
-    });
-  });
-})();
+const txtDirectories = [
+  "Mercules904",
+  "PandaPaxxy",
+  "AyyItsChevy",
+  "blueberries-dot-gg",
+  "YeezyGT",
+  // "misc",
+];
 
-// given an array of [valid roll, invalid roll, valid roll, invalid roll]
-// returns           [false,      1,            false,      3           ]
-// so i guess you have an array of the indices of invalid rolls
-function getInvalidWishlistRolls(rolls: WishListRoll[], verbose = false) {
-  verbose && console.log(`about to check ${rolls.length} rolls`);
-  const errorsByItem: { [key: string]: Set<string> } = { badItems: new Set() };
+const txtFilePaths = txtDirectories.flatMap((dirName) =>
+  fs
+    .readdirSync("../" + dirName)
+    .filter((fileName) => fileName.endsWith(".txt"))
+    .map((fileName) => "../" + dirName + "/" + fileName)
+);
 
-  const indicesOfBadRolls = rolls.map((roll, index) => {
-    const perksOnThisRoll = [...roll.recommendedPerks];
-    const rollItem = getItem(roll.itemHash);
-    if (!rollItem) {
-      errorsByItem.badItems.add(
-        `!!! couldn't find an item for ${roll.itemHash} !!!`
-      );
-      return;
+const remoteTxts = [
+  "https://raw.githubusercontent.com/Butlins12/destiny-rolls/main/DMB-inspired-rolls.txt",
+  "https://raw.githubusercontent.com/Butlins12/destiny-rolls/main/S15-IB-new-rolls-DMB-ep230.txt",
+];
+
+for (const path of [...txtFilePaths, ...remoteTxts]) {
+  const plainText = path.startsWith("http")
+    ? await (await fetch(path)).text()
+    : fs.readFileSync(path, "utf-8");
+
+  let thisFileOutput = path.split("/").slice(-1)[0];
+
+  const rolls = toWishList(plainText).wishListRolls;
+  thisFileOutput = ` (${rolls.length}) `.padStart(8) + thisFileOutput;
+  let thisFileBadCount = 0;
+  const uniqueErrors = new Set<string>();
+  for (const roll of rolls) {
+    const rollError = isInvalidRoll(roll);
+    if (rollError) {
+      thisFileBadCount++;
+      uniqueErrors.add(rollError);
     }
-    const itemName = rollItem.displayProperties.name;
-    if (isDummy(rollItem)) {
-      errorsByItem.badItems.add(
-        `!!! ${itemName} (${roll.itemHash}) is a dummy item !!!`
-      );
-      return;
-    }
+  }
+  counter += thisFileBadCount;
 
-    const perksThisGunCanActuallyHave = (rollItem.sockets?.socketEntries
-      .reduce((acc, se) => {
-        const hashesInPlugsets = placesToLookForValidPerks.reduce(
-          (inneracc, key) =>
-            inneracc.concat(
-              getPlugSet(se[key] ?? -99999999)?.reusablePlugItems?.map(
-                p => p.plugItemHash
-              )
-            ),
-          [] as (number | undefined)[]
-        );
-        const reusablePlugItemHashes = se.reusablePlugItems.map(
-          pi => pi.plugItemHash
-        );
-        return acc.concat([...hashesInPlugsets, ...reusablePlugItemHashes]);
-      }, [] as (number | undefined)[])
-      .filter(Boolean) ?? []) as number[];
-    const allPerksAreValid = perksOnThisRoll.every(p =>
-      perksThisGunCanActuallyHave.includes(p)
+  thisFileOutput = `${thisFileBadCount ? "✘" : "✓"} ` + thisFileOutput;
+  if (uniqueErrors.size)
+    thisFileOutput += ("\n" + [...uniqueErrors].join("\n")).replace(
+      /\n/g,
+      "\n          "
     );
-    if (allPerksAreValid) {
-      // verbose && console.log("that was a valid roll for " + itemName);
-      return false;
-    }
-    // verbose && console.log("something is wrong with " + itemName + ":");
-
-    perksOnThisRoll.forEach(p => {
-      if (perksThisGunCanActuallyHave.includes(p)) return;
-
-      const cantExistPerkName = getItem(p)?.displayProperties.name;
-      const maybeMeant = perksThisGunCanActuallyHave
-        .filter(a => a !== p)
-        .find(a => getItem(a)?.displayProperties.name === cantExistPerkName);
-      if (!errorsByItem[itemName]) errorsByItem[itemName] = new Set();
-      errorsByItem[itemName].add(
-        `${cantExistPerkName} (${p}) cant exist on ${itemName} (${roll.itemHash})` +
-          (!maybeMeant
-            ? ""
-            : `\n    maybe you meant rollable perk ${cantExistPerkName} (${maybeMeant})?`)
-      );
-    });
-
-    return index;
-  });
-  Object.values(errorsByItem).forEach(set =>
-    set.forEach(err => verbose && console.log(err))
-  );
-  return indicesOfBadRolls;
-}
-
-// ItemCategory [3109687656] "Dummies"
-function isDummy(item: DestinyInventoryItemDefinition) {
-  return item?.itemCategoryHashes?.includes(3109687656);
-}
-function getItem(hash: number) {
-  return d2manifest.get("DestinyInventoryItemDefinition", hash);
-}
-function getPlugSet(hash: number) {
-  return d2manifest.get("DestinyPlugSetDefinition", hash);
+  console.log(thisFileOutput);
 }
